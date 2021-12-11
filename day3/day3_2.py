a3507c26606eb447a431463f1debb1c6c2cf4f3b
Wonjin Park <wjpark11@gmail.com>
2021-12-11 22:12:17 +0900

    finish day3_2

--- day3/day3_2.py
+++ day3/day3_2.py
@@ -2,37 +2,29 @@ with open("day3_input.txt", "rt") as f:
     inputs = f.readlines()
     inputs = [input.strip() for input in inputs]
 
-oxygen_gen_list = [0 for _ in range(12)]
-for bits in inputs:
-    idx = 0
-    for bit in bits:
-        if bit == "1":
-            oxygen_gen_list[idx] += 1
-        else:
-            oxygen_gen_list[idx] -= 1
-        idx += 1
+filtered_list = inputs
+for idx in range(12):
+    digit = '0'
+    occur_list = [1 if input[idx]=='1' else -1 for input in filtered_list]
+    if sum(occur_list) >= 0:
+        digit = '1'
+    else:
+        digit = '0'
+    filtered_list = [item for item in filtered_list if item[idx]==digit]
+    if len(filtered_list) == 1:
+        oxygen_gen_rating = int('0b'+''.join(filtered_list[0]),2)
 
-oxygen_gen_code = ''.join(["1" if item>=0 else "0" for item in oxygen_gen_list])
 
-oxygen_gen_rating = ''
-for i in range(12):
-    oxygen_gen_partial = oxygen_gen_code[:i+1]
-    oxygen_match_list = [code for code in inputs if code[:i+1]==oxygen_gen_partial]    
-    if len(oxygen_match_list) == 1:
-        oxygen_gen_rating = oxygen_match_list[0]
-        break
+filtered_list = inputs
+for idx in range(12):
+    digit = '0'
+    occur_list = [1 if input[idx]=='1' else -1 for input in filtered_list]
+    if sum(occur_list) < 0:
+        digit = '1'
+    else:
+        digit = '0'
+    filtered_list = [item for item in filtered_list if item[idx]==digit]    
+    if len(filtered_list) == 1:
+        co2_scr_rating = int('0b'+''.join(filtered_list[0]),2)
 
-co2_scr_code = ''.join(["1" if item < 0 else "0" for item in oxygen_gen_list])
-
-co2_scr_rating = ''
-for i in range(12):
-    co2_scr_partial = co2_scr_code[:i+1]
-    co2_match_list = [code for code in inputs if code[:i+1]==co2_scr_partial]
-    print(co2_match_list)
-    if len(co2_match_list) == 1:
-        co2_scr_rating = co2_match_list[0]
-        break
-
-print(oxygen_gen_rating)
-print(co2_scr_rating)
-# print (int('0b'+oxygen_gen_rating,2) * int('0b'+co2_scr_rating,2))
+print(oxygen_gen_rating*co2_scr_rating)
